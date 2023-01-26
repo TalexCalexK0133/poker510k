394637970404309c11e12c67dc6cec9b2f86dde7
TalexCK <84849086+TalexCalexK0133@users.noreply.github.com>
2023-01-26 15:37:08 +0800

    Add files via upload

--- src/main/java/com/talexck/poker510k/GameLogic/showpoker.java
+++ src/main/java/com/talexck/poker510k/GameLogic/showpoker.java
@@ -1,7 +1,7 @@
 package com.talexck.poker510k.GameLogic;
 
+import javax.swing.*;
 import java.util.ArrayList;
-import com.talexck.poker510k.GameLogic.givepoker.*;
 
 import static com.talexck.poker510k.GameLogic.givepoker.pokers1;
 import static com.talexck.poker510k.GameLogic.givepoker.pokers2;
@@ -13,7 +13,48 @@ public class showpoker {
     public static ArrayList pokered2 = new ArrayList<String>();
     public static ArrayList pokered3 = new ArrayList<String>();
     public static ArrayList pokered4 = new ArrayList<String>();
+    public static Integer ifcalled;//0-没有 1-单张 2-方片一对 3-草花一对 4-红桃一对 5-黑桃一对 6-小王一对 7-大王一对 8-红心5一对 9-四张王
+    public static Integer poker;
+    public static Integer callmain(Integer poker1, Integer poker2){
+        if(poker2==0/*单张*/){
+            if(ifcalled<1){
+                ifcalled=1;
+                poker = poker1;
+            }
+        }
+        if(poker2!=0/*一对*/){
+            if(poker1==4 && poker2==4 &&ifcalled<2){
+                ifcalled=2;
+                poker=4;
+            }else if(poker1==3 && poker2==3 &&ifcalled<3){
+                ifcalled=3;
+                poker=3;
+            }else if(poker1==2 && poker2==2 &&ifcalled<4){
+                ifcalled=4;
+                poker=2;
+            }else if(poker1==1 && poker2==1 &&ifcalled<5){
+                ifcalled=5;
+                poker=1;
+            }else if(poker1==51 && poker2==51 &&ifcalled<6){
+                ifcalled=6;
+                poker=5;
+            }else if(poker1==52 && poker2==52 &&ifcalled<7){
+                ifcalled=7;
+                poker=5;
+            }else if(poker1==205 && poker2==205 &&ifcalled<8){
+                ifcalled=8;
+                String mainpoker = JOptionPane.showInputDialog(null,"1黑2红3梅4方5无主","点主",JOptionPane.PLAIN_MESSAGE);
+                poker = Integer.valueOf(mainpoker).intValue();
+            }else if(poker1==51 && poker2==2 /*一大一小*/&&ifcalled<9){
+                ifcalled=9;
+                String mainpoker = JOptionPane.showInputDialog(null,"1黑2红3梅4方5无主","点主",JOptionPane.PLAIN_MESSAGE);
+                poker = Integer.valueOf(mainpoker).intValue();
+            }
+        }
+        return poker;
+    }
     public static void showpokers(Integer fister, Integer condition) throws InterruptedException {
+        ifcalled = 0;
         if(condition==1){//抢台
             Integer i = 0;
             while (i < 25) {
--- src/main/java/com/talexck/poker510k/GameMain.java
+++ src/main/java/com/talexck/poker510k/GameMain.java
@@ -5,8 +5,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static com.talexck.poker510k.GameLogic.showpoker.callmain;
+
 public class GameMain extends JFrame {
     private MenuItemActionListener menuItemAction=new MenuItemActionListener();
+    public static Integer mainpoker;
     public GameMain() throws InterruptedException {
         super("510k游戏");
 
@@ -27,7 +30,10 @@ public class GameMain extends JFrame {
         this.setResizable(false);
         this.setVisible(true);
         com.talexck.poker510k.GameLogic.givepoker.givepokers(1,1);
+        mainpoker = 2;
     }
+
+    public static Integer poker=0;
 
     class MenuItemActionListener extends JFrame  implements ActionListener {
         public void actionPerformed(ActionEvent e) {
@@ -35,9 +41,49 @@ public class GameMain extends JFrame {
             if("出牌".equals(actionCommand)){
 
             }else if("叫主/炒牌".equals(actionCommand)){
-
+                Integer poker1 = 0;//得到选牌
+                Integer poker2 = 0;
+                if (poker2==0 && poker1!=0 && poker1-Math.round(poker1/100)*100==mainpoker && poker==0){
+                    poker1 = Math.round(poker1/100);
+                    poker=callmain(poker1, poker2);
+                }else if(poker1==poker2 && poker1-Math.round(poker1/100)*100==mainpoker){
+                    poker1 = Math.round(poker1/100);
+                    poker2 = Math.round(poker1/100);
+                    Integer pokers = poker;
+                    poker=callmain(poker1, poker2);
+                    if(pokers==poker){
+                        //丢回去
+                    }
+                }else if(poker1==poker2 && poker1==551) {
+                    poker1 = Math.round(poker1 / 100);
+                    poker2 = Math.round(poker1 / 100);
+                    Integer pokers = poker;
+                    poker = callmain(poker1, poker2);
+                    if(pokers==poker){
+                        //丢回去
+                    }
+                }else if(poker1==poker2 && poker1==552) {
+                    poker1 = Math.round(poker1 / 100);
+                    poker2 = Math.round(poker1 / 100);
+                    Integer pokers = poker;
+                    poker = callmain(poker1, poker2);
+                    if(pokers==poker){
+                        //丢回去
+                    }
+                }else if(poker1==poker2 && poker1==205) {
+                    poker1 = 205;
+                    poker2 = 205;
+                    poker = callmain(poker1, poker2);
+                }
+                else if(poker1==551 && poker2==552) {
+                    poker1 = 551;
+                    poker2 = 552;
+                    poker = callmain(poker1, poker2);//这边丢过去叫主成功
+                }else{
+                    //丢回去
+                }
             }else if("造反".equals(actionCommand)) {
-
+                //暂不开发，一人无硬主且无软主时触发重发牌
             }
         }
     }
